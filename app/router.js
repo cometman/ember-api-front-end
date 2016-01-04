210442c32783c877855eaffaabea32b1561a8c39
cometman <csbuiss@gmail.com>
2016-01-04 10:52:22 -0600

    adding scaffolding for ember

--- app/router.js
+++ app/router.js
@@ -5,7 +5,11 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
+  this.resource('video_fragments', function(){
+    this.$resource('contact', {path: '/:contact_id'});
+  });
+  this.route('playlists', function() {});
 });
 
 export default Router;
