179f159c6578b2ac7c7d6f8a1ac4b2eec0bb07b8
Clayton Selby <csbuiss@gmail.com>
2016-01-05 12:52:36 -0600

    Adding section for uploading video fragments

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ export default Router.map(function() {
   this.route('playlists', function(){
     this.route('show', {path: '/:playlist_id'});
   });
+  this.route('videoFragment');
 });
 
 // export default Router;
